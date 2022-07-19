2b9ba520cdedf46bb4d6c7a20cd9d3a36c839417
Elbek Abdurahimov <abdurahimovelbek111@gmail.com>
2022-07-19 21:26:56 +0500

    Event and File work

--- Delegatelar bilan ishlash/Program.cs
+++ Delegatelar bilan ishlash/Program.cs
@@ -1,6 +1,65 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Delegatelar_bilan_ishlash;
-mbox:
+#region Fayllar bilan ishlash
+
+// Fayl yaratish
+/*FileInfo file = new FileInfo(@"C:\Apple.txt");
+FileStream stream = file.Create();
+Console.WriteLine($"File Creation Time : {file.CreationTime}");*/
+
+//Faylni uchirish
+/*FileInfo file = new FileInfo(@"C:\Apple.txt");
+file.Delete();
+Console.WriteLine($"File Delete : file remove");*/
+
+
+//Fayllarni diskdan uqish
+/*string path = @"C:\Users\Администратор\source\repos\SuniyIntellekt_IAT4\SuniyIntellekt_IAT4\bin\Debug\ABC";
+DirectoryInfo directory = new DirectoryInfo(@"C:\");
+DirectoryInfo[] dirs = directory.GetDirectories();
+Console.WriteLine($"File path : {directory.FullName}\n" +
+    $"File Name : {directory.Name}\n" +
+    $"File Create Time : {directory.CreationTime}\n" +
+    $"File Open Time : {directory.LastAccessTime}\n" +
+    $"File folder or file : {directory.Attributes}\n" +
+    $"File parent : {directory.Parent}\n" +
+    $"File CreationTimeUtc : {directory.CreationTimeUtc}\n" +
+    $"File Exists : {directory.Exists}\n" +
+    $"File Extension : {directory.Extension}\n" +
+    $"File last write time : {directory.LastWriteTime}\n" +
+    $"File  : {directory.LastWriteTimeUtc}");
+Console.WriteLine("-------------------------------------------------------------------------");
+foreach (var item in dirs)
+{
+    Console.WriteLine($"File path : {item.FullName} || " +
+     $"File Name : {item.Name} || " +
+     $"File Create Time : {item.CreationTime} || " +
+     $"File Open Time : {item.LastAccessTime} || " +
+     $"File folder or file : {item.Attributes} || " +
+     $"File parent : {item.Parent} || " +
+     $"File CreationTimeUtc : {item.CreationTimeUtc} || " +
+     $"File Exists : {item.Exists} || " +
+     $"File Extension : {item.Extension} || " +
+     $"File last write time : {item.LastWriteTime} || File number : {item.ToString().Length} || " +
+     $"File  : {item.LastWriteTimeUtc} || File Atrubut : {item.Attributes.ToString()}{Environment.NewLine}");
+}
+*/
+#endregion
+
+
+#region Eventlar bilan ishlash
+/*static void Tugma_Click()
+{
+    Console.WriteLine("Tugma bosildi");
+}
+Tugma tugma=new Tugma();
+//tugma.Click +=new TugmaDelegate(Tugma_Click);
+tugma.Click += Tugma_Click;
+tugma.Simulation();
+tugma.Click-=new ClicEventHandler(Tugma_Click);*/
+#endregion
+#region Delegate bilan ishlash
+/*mbox:
 Console.WriteLine("Kiritayotgan pul miqdoriz dollor bulsa 0, so'm bo'lsa 1 kiriting :");
 int val=int.Parse(Console.ReadLine());
 switch (val)
@@ -29,5 +88,5 @@ switch (val)
             "Iltimos qayrtadan kiriting!!");
         goto mbox;
         break;
-}
-
+}*/
+#endregion
---
+++ Delegatelar bilan ishlash/Tugma.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegatelar_bilan_ishlash
+{
+    public delegate void ClicEventHandler();
+    public class Tugma
+    {
+        public event ClicEventHandler Click;
+        public void Simulation()
+        {
+            if (Click!=null)
+            {
+                Click();
+            }
+            else
+            {
+                Console.WriteLine("Hodisa ro'y bermadi!!!");
+            }
+        }
+    }
+}
